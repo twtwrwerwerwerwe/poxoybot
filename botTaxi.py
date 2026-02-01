cba62e73aae338672d83bf46aaff78bc18f9d06d
Rozimuhammad Akramjonov <rozimuhammad@example.com>
2026-02-01 14:17:56 +0500

    yangilandi

--- botTaxi.py
+++ botTaxi.py
@@ -139,12 +139,12 @@ async def handler(event):
         phone_display = phone if phone else "Berkitilgan"
 
         message_text = (
-            f"🚖 <b>Hamroh taxi</b>\n"
-            f"📝 <b></b> {text}\n"
-            f"📍 <b>Guruh:</b> {group_display}\n"
-            f"👤 <b></b> {owner_display}\n"
-            f"📞 <b></b> {phone_display}\n"
-            f"🔗 <b>Profilga o'tish:</b> {profile_link}"
+            f"🚖 <b>XAMROH TAXI</b>\n\n"
+            f"📝 <b></b> {text}\n\n"
+            f"📍 <b>Guruh:</b> {group_display}\n\n"
+            f"👤 <b></b> {owner_display}\n\n"
+            f"📞 <b></b> {phone_display}\n\n"
+            f"🔗 <b></b> {profile_link}"
         )
 
 
